567627df7d8450972d5c38843e829cd69a5dcc8a
Nabatanzi Gorret <nabatanzigorret143@gmail.com>
2024-10-21 08:59:24 +0300

    Added user and loan application repositories

---
+++ backend/src/main/java/loanapplications/spring/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package loanapplications.spring.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import loanapplications.spring.models.User;
+
+
+public  interface UserRepository extends JpaRepository<User, Long>{
+    
+    Optional<User> findByEmail(String email);
+    Optional<User> findByContact(String contact);
+
+}
