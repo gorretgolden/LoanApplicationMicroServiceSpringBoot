b52900934defea053d06df92309379288c3b507a
Nabatanzi Gorret <nabatanzigorret143@gmail.com>
2024-10-22 09:04:47 +0300

    added findByName to user repo

--- backend/src/main/java/loanapplications/spring/security/SecurityConfig.java
+++ backend/src/main/java/loanapplications/spring/security/SecurityConfig.java
@@ -1,10 +1,10 @@
 package loanapplications.spring.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
@@ -12,13 +12,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable() // Disable CSRF for simplicity (not recommended for production)
-            .authorizeRequests()
-            .antMatchers("/api/auth/register", "/api/auth/login").permitAll() // Allow access to registration and login endpoints
+        http.csrf().disable() 
+            .authorizeHttpRequests() 
+            .requestMatchers("/api/auth/register", "/api/auth/login").permitAll() // Allowing access to registration and login endpoints
             .anyRequest().authenticated(); // Require authentication for any other request
-        
+
         return http.build();
     }
-
-    
 }
